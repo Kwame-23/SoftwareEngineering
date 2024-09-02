f7e9dd652c1828a797b9a23869f8e1a1f66029c2
Kwame-23 <137163770+Kwame-23@users.noreply.github.com>
2024-09-02 08:58:38 +0000

    db changes

--- settings/connection.php
+++ settings/connection.php
@@ -4,7 +4,7 @@
 define('DB_HOST', 'localhost');
 define('DB_USER', 'kwame');
 define('DB_PASS', 'Kwame23');
-define('DB_NAME', 'roboxoxo');
+define('DB_NAME', 'RoboXoxo');
 
 // Connect to the database using mysqli
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME) or die("Couldn't connect to database");
